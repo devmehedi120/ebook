6f3640f90145a154cee74841a27884b6539da419
devmehedi120 <personal.mehedi@outlook.com>
2024-05-07 16:32:35 +0600

    initialized the plugin

---
+++ includes/class-mh-ebook-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wodevmehedi.com
+ * @since      1.0.0
+ *
+ * @package    Mh_Ebook
+ * @subpackage Mh_Ebook/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Mh_Ebook
+ * @subpackage Mh_Ebook/includes
+ * @author     Mehedi Hasan <wpdevmehedi@gmail.com>
+ */
+class Mh_Ebook_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-mh-ebook-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://wodevmehedi.com
+ * @since      1.0.0
+ *
+ * @package    Mh_Ebook
+ * @subpackage Mh_Ebook/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Mh_Ebook
+ * @subpackage Mh_Ebook/includes
+ * @author     Mehedi Hasan <wpdevmehedi@gmail.com>
+ */
+class Mh_Ebook_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
